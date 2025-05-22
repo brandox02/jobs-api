8a1e89a52142f00e0812298e6e808570f331339e
brandox02 <brandox02.dev@gmail.com>
2025-05-21 20:09:53 -0400

    allowing cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
   // process.env.TZ = 'America/Santo_Domingo';
   const app = await NestFactory.create(AppModule);
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: '*', // allows all origins
+  });
+
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT);
