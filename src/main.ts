e4956aae4fa8305bc3e2ddd4e0267e15d83941d4
brandox02 <brandox02.dev@gmail.com>
2025-05-21 20:24:25 -0400

    testing cors

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ async function bootstrap() {
   // process.env.TZ = 'America/Santo_Domingo';
   const app = await NestFactory.create(AppModule);
   // Enable CORS for all origins
+
+  console.log("Testing cors")
   app.enableCors({
-    origin: '*', // allows all origins
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
   });
 
   app.use(json({ limit: '50mb' }));
